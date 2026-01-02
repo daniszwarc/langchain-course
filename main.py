0e664c2cce2db1b70f8930412508aa164fa4208b
Dani Szwarc <dani@thiez.com>
2026-01-02 17:06:01 -0500

    Create React Agent

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt
+
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
+chain = agent_executor
+
+
 def main():
-    print("Hello from langchain-course!")
+    result = chain.invoke(
+        input={
+            "input": "Search for 3 jobs postings for an ai engineer using Langchain in the Montreal area on LinkedIn and list their details"
+        }
+    )
+    print(result)
 
 
 if __name__ == "__main__":
