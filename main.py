7349c3b9c8ef35acc1b4045d2d7d979b74c2df01
Dani Szwarc <dani@thiez.com>
2026-01-08 15:05:48 -0500

    Conversion to V1

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-from langchain_classic import hub
-from langchain_classic.agents import AgentExecutor
-from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableLambda
+from langchain.agents import create_agent
+from langchain.agents.structured_output import ToolStrategy
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4o")
-structured_llm = llm.with_structured_output(AgentResponse)
-react_prompt = hub.pull("hwchase17/react")
-react_prompt_with_format_instructions = PromptTemplate(
-    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables=["input", "agent_scratchpad", "tool_names"],
-).partial(format_instructions="")
 
-agent = create_react_agent(
-    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    response_format=ToolStrategy(AgentResponse)
 )
-
-agent_executor = AgentExecutor(
-    agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
-)
-extract_output = RunnableLambda(lambda x: x["output"])
-chain = agent_executor | extract_output | structured_llm
 
 
 def main():
-    result = chain.invoke(
-        input={
-            "input": "Search for 3 jobs postings for GenAI with Langchain in the Montreal area on LinkedIn and list their details"
+    result = agent.invoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Search for 3 jobs postings for Agentic AI, GenAI with Langchain in the Montreal area on LinkedIn and list their details"
+                }
+            ]
         }
     )
-    print(result)
+    structured = result.get("structured_response", None)
+    print(structured if structured is not None else result)
 
 
 if __name__ == "__main__":
